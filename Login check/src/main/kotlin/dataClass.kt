88b122b9a449e5658ea862308664a6fe87d1fc48
saipreetham2258 <preetham.r@techouts.com>
2024-11-15 18:11:36 +0530

    Add files via upload

---
+++ Login check/src/main/kotlin/dataClass.kt
@@ -0,0 +1,11 @@
+package org.example
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class User2(val name : String,
+                 val Mobilenumber : Long,
+                 val age : Int,
+                 val Id : Int,
+                 val mail : String,
+                 val password : String)
